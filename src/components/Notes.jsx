7fc0ac72bc2cfaa8881ec333572fd30033508313
LuisCrespoURU <99094598+LuisCrespoURU@users.noreply.github.com>
2024-05-31 01:51:17 -0400

    Comentario de prueba
    ._.

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,7 +3,7 @@ import "../css/Notes.css"
 function Notes() {
   return (
     <div className="container-notes">
-        <h2>hola</h2>
+      <h2>En nombre del alcalde del Municipio Siglo XXI, manifestó públicamente que la entidad no está dispuesta a hablar de, mantener los pasajes gratuitos para personas mayores de 60 años, personas con discapacidad y estudiantes. Esto se hizo por motivos políticos y estaban dispuestos a cancelarlo, a pesar del impacto en los ingresos de la empresa. Desaprueba el aumento de tarifas. La oficina del alcalde se negó a autorizar un aumento de tarifas, temiendo que pareciera dar una ventaja a los inversores extranjeros y perjudicar al público, especialmente meses antes de las elecciones municipales. Mantener el control sobre las decisiones sobre tipos de interés. La alcaldía quiere conservar la autoridad para aprobar o rechazar aumentos de tarifas, sin ceder esa autoridad a empresas privadas. No suspender ni despedir a 50 conductores que actualmente están suspendidos o de licencia.</h2>
     </div>
   )
 }

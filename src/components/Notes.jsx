b99e238a0da8eb556503593cc7b64d7e0291606f
LuisJURU <luis.30200228@uru.edu>
2024-05-31 23:08:51 -0400

    90% ._.

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -3,6 +3,9 @@
 import { useState } from "react";
 import "../css/Notes.css";
 
+import Delete from "./Delete";
+import Update from "./Update";
+
 function Notes({ notes }) {
   const [showPopup, setShowPopup] = useState(false);
   const [selectedNote, setSelectedNote] = useState(null);
@@ -18,23 +21,33 @@ function Notes({ notes }) {
 
   return (
     <div className="container-notes">
-      {notes.length > 0 && notes.map((note, index) => (
-        note.text && note.text.trim() !== "" && (
-          <div key={index}>
-            <div className="notes" onClick={() => handleClick(note)}>
-              <div>{note.text}</div>
-            </div>
-          </div>
-        )
-      ))}
-      {showPopup && 
+      {notes.length > 0 &&
+        notes.map(
+          (note, index) =>
+            note.text &&
+            note.text.trim() !== "" && (
+              <div
+                key={index}
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <div className="notes" onClick={() => handleClick(note)}>
+                  <div>{note.text}</div>
+                </div>
+                <div className="actions">
+                  <Delete />
+                  <Update />
+                </div>
+              </div>
+            )
+        )}
+      {showPopup && (
         <div className="overlay">
           <div className="popup">
             <button onClick={handleClose}>Cerrar</button>
             <p>{selectedNote.text}</p>
           </div>
         </div>
-      }
+      )}
     </div>
   );
 }
